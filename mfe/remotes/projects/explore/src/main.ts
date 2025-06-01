9cf9da1620c97a4e885ce20f79021e193bfa5afd
Aukevanoost <auke.v.o@hotmail.com>
2025-06-01 20:36:45 +0200

    fix: moved remotes

--- mfe/remotes/projects/explore/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { initFederation } from '@angular-architects/native-federation';
-
-initFederation()
-  .catch(err => console.error(err))
-  .then(_ => import('./exp-recommendations/recommendations.bootstrap'))
-  .then(_ => import('./exp-teasers/teasers.bootstrap'))
-  .catch(err => console.error(err));
