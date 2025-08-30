9bfaae4a037843389e6c2ca7a0e697a2ad95ba91
Aukevanoost <auke.v.o@hotmail.com>
2025-08-30 13:35:04 +0200

    fix: Added caching orchestrator

--- src/caching/orchestrator.ts
+++ src/caching/orchestrator.ts
@@ -2,17 +2,24 @@ import "es-module-shims";
 
 import { initFederation } from "vanilla-native-federation";
 import {
+  consoleLogger,
   NFOptions,
-  useShimImportMap,
   sessionStorageEntry,
+  useShimImportMap,
 } from "vanilla-native-federation/options";
 
 (async () => {
   try {
     const feedServiceUrl = document.querySelector(`meta[name="piral"]`)?.getAttribute("content")!;
 
-    const { loadRemoteModule } = await initFederation(feedServiceUrl, {
+    const { loadRemoteModule, initRemoteEntry } = await initFederation(feedServiceUrl, {
       logLevel: "debug",
+      logger: consoleLogger,
+      profile: {
+        latestSharedExternal: false,
+        overrideCachedRemotes: "init-only",
+        overrideCachedRemotesIfURLMatches: true,
+      },
       storage: sessionStorageEntry,
       ...useShimImportMap({ shimMode: true }),
     } as NFOptions);
@@ -21,6 +28,7 @@ import {
       new CustomEvent("mfe-loader-available", {
         detail: {
           loadRemoteModule,
+          initRemoteEntry,
         },
       })
     );
